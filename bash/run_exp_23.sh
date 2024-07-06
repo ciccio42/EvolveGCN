6a5116f49203b33f95717b5e097b0eb77f9090eb
Alepao99 <alessandropaolillo67@gmail.com>
2024-07-06 18:42:10 +0200

    Update feature importances experiment

--- bash/run_exp_23.sh
+++ bash/run_exp_23.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=3
 
-BASE_PATH="/user/apaolillo/gnn-network-analysis/dynamic_graphs/EvolveGCN"
+BASE_PATH="/user/apaolillo/dynamic_graphs/EvolveGCN/"
 
 configurations=(
     
+    "150k_IoT23_top23"
+    "120k_IoT23_top23"
     "90k_IoT23_top23"
+    "60k_IoT23_top23"
 )
 
 cd "$BASE_PATH"
@@ -15,23 +18,23 @@ for ((i=0; i<${#configurations[@]}; i+=1)); do
     DATA_PATH="${configurations[i]}"
     
     echo "EGCN-O norm"
-    YAML_FILE="./experiments/$DATA_PATH/parameters_egcn_o_anomaly_norm.yaml"
+    YAML_FILE="./experiments/feature_importance/$DATA_PATH/parameters_egcn_o_anomaly_norm.yaml"
     python run_exp_anomaly.py --config_file "$YAML_FILE"
     
     echo "EGCN-H norm"
-    YAML_FILE="./experiments/$DATA_PATH/parameters_egcn_h_anomaly_norm.yaml"
+    YAML_FILE="./experiments/feature_importance/$DATA_PATH/parameters_egcn_h_anomaly_norm.yaml"
     python run_exp_anomaly.py --config_file "$YAML_FILE"
 
     echo "LSTM_A norm"
-    YAML_FILE="./experiments/$DATA_PATH/parameters_lstmA_anomaly_norm.yaml"
+    YAML_FILE="./experiments/feature_importance/$DATA_PATH/parameters_lstmA_anomaly_norm.yaml"
     python run_exp_anomaly.py --config_file "$YAML_FILE"
 
     echo "GRU-A norm"
-    YAML_FILE="./experiments/$DATA_PATH/parameters_gruA_anomaly_norm.yaml"
+    YAML_FILE="./experiments/feature_importance/$DATA_PATH/parameters_gruA_anomaly_norm.yaml"
     python run_exp_anomaly.py --config_file "$YAML_FILE"
 
     echo "GCN norm"
-    YAML_FILE="./experiments/$DATA_PATH/parameters_gcn_anomaly_norm.yaml"
+    YAML_FILE="./experiments/feature_importance/$DATA_PATH/parameters_gcn_anomaly_norm.yaml"
     python run_exp_anomaly.py --config_file "$YAML_FILE"
 done
 
--- bash/run_exp_only_etdg.sh
+++ bash/run_exp_only_etdg.sh
@@ -5,7 +5,10 @@ BASE_PATH="/user/apaolillo/gnn-network-analysis/dynamic_graphs/EvolveGCN"
 
 configurations=(
     
+    "150k_IoT23_only_etdg"
+    "120k_IoT23_only_etdg"
     "90k_IoT23_only_etdg"
+    "60k_IoT23_only_etdg"
 )
 
 cd "$BASE_PATH"
@@ -15,23 +18,23 @@ for ((i=0; i<${#configurations[@]}; i+=1)); do
     DATA_PATH="${configurations[i]}"
     
     echo "EGCN-O norm"
-    YAML_FILE="./experiments/$DATA_PATH/parameters_egcn_o_anomaly_norm.yaml"
+    YAML_FILE="./experiments/feature_importance/$DATA_PATH/parameters_egcn_o_anomaly_norm.yaml"
     python run_exp_anomaly.py --config_file "$YAML_FILE"
     
     echo "EGCN-H norm"
-    YAML_FILE="./experiments/$DATA_PATH/parameters_egcn_h_anomaly_norm.yaml"
+    YAML_FILE="./experiments/feature_importance/$DATA_PATH/parameters_egcn_h_anomaly_norm.yaml"
     python run_exp_anomaly.py --config_file "$YAML_FILE"
 
     echo "LSTM_A norm"
-    YAML_FILE="./experiments/$DATA_PATH/parameters_lstmA_anomaly_norm.yaml"
+    YAML_FILE="./experiments/feature_importance/$DATA_PATH/parameters_lstmA_anomaly_norm.yaml"
     python run_exp_anomaly.py --config_file "$YAML_FILE"
 
     echo "GRU-A norm"
-    YAML_FILE="./experiments/$DATA_PATH/parameters_gruA_anomaly_norm.yaml"
+    YAML_FILE="./experiments/feature_importance/$DATA_PATH/parameters_gruA_anomaly_norm.yaml"
     python run_exp_anomaly.py --config_file "$YAML_FILE"
 
     echo "GCN norm"
-    YAML_FILE="./experiments/$DATA_PATH/parameters_gcn_anomaly_norm.yaml"
+    YAML_FILE="./experiments/feature_importance/$DATA_PATH/parameters_gcn_anomaly_norm.yaml"
     python run_exp_anomaly.py --config_file "$YAML_FILE"
 done
 
